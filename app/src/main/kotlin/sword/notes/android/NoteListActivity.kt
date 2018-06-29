d4913c26178f0670f611f0b303f87dfc5ad7f729
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2018-06-29 11:53:14 +0200

    Allow note creation by its current time millis

--- app/src/main/kotlin/sword/notes/android/NoteListActivity.kt
+++ app/src/main/kotlin/sword/notes/android/NoteListActivity.kt
@@ -2,19 +2,57 @@ package sword.notes.android
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ListView
+import java.io.File
 
 class NoteListActivity : Activity() {
+
+    val notesDirName = "notes"
 
     val listView by lazy {
         findViewById<ListView>(R.id.listView)
     }
 
+    val notesDir: File by lazy {
+        val notesDir = File(filesDir, notesDirName)
+        notesDir.mkdir()
+        notesDir
+    }
+
+    private fun getNotesInfo() : List<NoteListItem> {
+        return notesDir.list().map({ name -> NoteListItem(name) })
+    }
+
+    private fun createNote(name: String) {
+        if (!File(notesDir, name).createNewFile()) {
+            throw AssertionError()
+        }
+    }
+
+    private fun updateList() {
+        listView.adapter = NoteListAdapter(getNotesInfo())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.note_list_activity)
+    }
 
-        val notes = listOf(NoteListItem("Note 1"), NoteListItem("Note 2"))
-        listView.adapter = NoteListAdapter(notes)
+    override fun onResume() {
+        super.onResume()
+        updateList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu!!.add("New")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        createNote(System.currentTimeMillis().toString())
+        updateList()
+        return true
     }
 }
